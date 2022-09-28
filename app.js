bdf87ee28bd6dc88247773de50cf29bffd9cde08
michaelx78 <michael_w_xu@brown.edu>
2022-09-28 19:59:16 -0400

    Done after forking thx for nothing TA staff

--- app.js
+++ app.js
@@ -26,26 +26,32 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const protein = fruitsArray.filter(checkProtein);
+
+  let names = protein.map(function(element){
+    return `${element.name}`;
+})
 
   // TODO: Create a new HTML element to display your data
 
   // TODO: Append your new element to the page
+  const newElement = document.createElement('div');
+  console.log(names);
+  newElement.innerHTML = names;
+
+  // Append the new element to an existing part of the webpage
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
 
 }
 
-// SAMPLE CODE of how to create and append a new HTML element to the page
-const exampleAddElement = () => {
-  // Create a new HTML element and set its properties
-  const newElement = document.createElement('div');
-  newElement.innerHTML = "this text is inside a div";
-
-  // Append the new element to an existing part of the webpage
-  const existingElement = document.getElementById('example-id');
-  existingElement.append(newElement);
+function checkProtein(fruit) {
+  // console.log(fruit.family);
+  return fruit.nutritions.protein > 0;
 }
 
 /**
